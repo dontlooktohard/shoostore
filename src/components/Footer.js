e683c11b5de35eb4aedd13b45d22113c8aa3e345
Your Name01echo <xitamarfx@gmail.com>
2023-02-27 11:18:28 +0200

    change repo

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/App.css";
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>shopping App&copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/App.css";
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <h1>Shopping App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
